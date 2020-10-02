0d0ec42c6ae779c55174c6503b849487da7dd126
Lindsey Gobert <lmgobert1@gmail.com>
2020-10-01 23:46:40 -0500

    Updates

---
+++ precache-manifest.bd8412f4dc5c565206bd17435c92cf8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b9b5d2f7bf2761ba7fe27634ab40b3",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "402818d8077e3e3e4bd7",
+    "url": "/User-Directory/static/js/2.71d640e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.71d640e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6470581b8c562ac374e4",
+    "url": "/User-Directory/static/js/main.88c51e3a.chunk.js"
+  },
+  {
+    "revision": "f8d550d65fe1b61ea334",
+    "url": "/User-Directory/static/js/runtime-main.b3b293a7.js"
+  }
+]);
